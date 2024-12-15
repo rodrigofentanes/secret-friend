8c0bc98ee290223163b75fc974d72dd63bc23fbc
Rodrigo Fentanes <rodrigo.fentanes@hotmail.com>
2024-12-14 23:06:52 -0300

    teste de envio de email pela vercel

--- js/app.js
+++ js/app.js
@@ -10,31 +10,28 @@
 // function fileWritten() {
 //   console.log('File written!');
 // }
+import mailTransport from transporter;
 
-function sendEmail() {
-  const nodemailer = require("nodemailer");
-
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'fentanesink@gmail.com',
-      pass: 'micv hrod wafc ghqz'
-    }
-  });
-
-  const mailOptions = {
-    from: 'fentanesink@gmail.com',
-    to: 'rodrigo.fentanes@hotmail.com',
-    subject: 'Sending Email using Node.js',
-    text: 'That was easy!'
-  };
+// function sendEmail() {
+const mailOptions = {
+  from: 'fentanesink@gmail.com',
+  to: 'rodrigo.fentanes@hotmail.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
   
   
-  transporter.sendMail(mailOptions, function(error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
-  });
-}
+//   transporter.sendMail(mailOptions, function(error, info) {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       console.log('Email sent: ' + info.response);
+//     }
+//   });
+// }
+
+export default defineEventHandler(async (event) => {
+  await mailTransport.sendMail(mailOptions);
+
+  return{}
+})
