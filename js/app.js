f4741dce1617ca1e5a2518a034dd8f0280372d46
Rodrigo Fentanes <rodrigo.fentanes@hotmail.com>
2024-12-14 22:03:45 -0300

    Teste Vercel and Express

---
+++ js/app.js
@@ -0,0 +1,37 @@
+// const fs = require('fs');  // This is the same then bellow code
+// import * as fs from 'fs';
+
+// console.log('Uuuuui!');
+
+// const words = ['unicorn', 'cupcake', 'rainbow'];
+// const output = words.join('\n');
+// fs.writeFile('words.txt', output, fileWritten);
+
+// function fileWritten() {
+//   console.log('File written!');
+// }
+
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'fentanesink@gmail.com',
+    pass: 'micv hrod wafc ghqz'
+  }
+});
+
+var mailOptions = {
+  from: 'fentanesink@gmail.com',
+  to: 'rodrigo.fentanes@hotmail.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
